4c2fa7c2c0462cfc85dc02460c28c37dd038217f
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2025-02-18 22:18:34 +0900

    refactor: ModerationController 응답 타입 BaseResponse → ResponseEntity로 변경 (WR9-23)

--- src/main/java/io/crops/warmletter/domain/moderation/controller/ModerationController.java
+++ src/main/java/io/crops/warmletter/domain/moderation/controller/ModerationController.java
@@ -5,6 +5,7 @@ import io.crops.warmletter.domain.moderation.dto.request.ModerationRequest;
 import io.crops.warmletter.domain.moderation.service.ModerationService;
 import io.crops.warmletter.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,9 @@ public class ModerationController {
     private final ModerationService moderationService;
 
     @PostMapping
-    public BaseResponse<String> createModerationWord(@RequestBody ModerationRequest request) {
+    public ResponseEntity<BaseResponse<String>> createModerationWord(@RequestBody ModerationRequest request) {
         moderationService.saveModerationWord(request);
-        return BaseResponse.of("검열단어 등록완료", "성공");
+        return ResponseEntity.ok(BaseResponse.of(request.getWord(), "성공"));
     }
 
 }

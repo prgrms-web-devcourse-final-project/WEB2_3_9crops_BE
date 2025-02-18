2070330cbe745fc88013eef0a572c75a8a0fac6c
고진영 <jim00011128@gmail.com>
2025-02-18 23:34:17 +0900

    remove: 변경 전 자바 코드 제거 (WR9-22)

--- src/main/java/io/crops/warmletter/domain/letter/controller/LettersControllerDocs.java
+++
@@ -1,4 +0,0 @@
-package io.crops.warmletter.domain.letter.controller;
-
-public interface LettersControllerDocs {
-}
--- src/main/java/io/crops/warmletter/domain/letter/repository/LettersRepository.java
+++
@@ -1,7 +0,0 @@
-package io.crops.warmletter.domain.letter.repository;
-
-import io.crops.warmletter.domain.letter.entity.Letter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LettersRepository extends JpaRepository<Letter, Long> {
-}

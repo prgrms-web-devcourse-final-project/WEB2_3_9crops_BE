bef28591f196c0a88ab93c7970a191357c570f18
leeys9423 <leeys9423@gmail.com>
2025-02-19 21:17:20 +0900

    test: test 프로필 환경에서 실행되도록 설정

--- src/test/java/io/crops/warmletter/WarmLetterApplicationTests.java
+++ src/test/java/io/crops/warmletter/WarmLetterApplicationTests.java
@@ -1,11 +1,14 @@
 package io.crops.warmletter;
 
+import io.crops.warmletter.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestConfig.class)
 class WarmLetterApplicationTests {
 
     @Test

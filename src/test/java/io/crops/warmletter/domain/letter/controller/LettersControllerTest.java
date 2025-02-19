615e8f3490758298eda3210ff68f465a555c64ea
고진영 <jim00011128@gmail.com>
2025-02-19 11:18:17 +0900

    fix: csrf 토큰 문제 active 변경으로 해결, local과 test 환경 db 분리 작업 (WR9-27)

--- src/test/java/io/crops/warmletter/domain/letter/controller/LettersControllerTest.java
+++ src/test/java/io/crops/warmletter/domain/letter/controller/LettersControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest
 class LettersControllerTest {

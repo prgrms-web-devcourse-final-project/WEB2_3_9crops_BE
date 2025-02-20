53cb1c73a4313f4a02cd04ffcb7c96354edbba11
HeoJeongHyeon <jh7e0n20@gmail.com>
2025-02-20 21:16:26 +0900

    feat Entity
    - SharePostDetail 관련 Entity 추가

---
+++ src/main/java/io/crops/warmletter/domain/share/dto/response/SharePostDetailResponse.java
@@ -0,0 +1,30 @@
+package io.crops.warmletter.domain.share.dto.response;
+
+import io.crops.warmletter.domain.letter.entity.Letter;
+import io.crops.warmletter.domain.member.entity.Member;
+import io.crops.warmletter.domain.share.entity.SharePost;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SharePostDetailResponse {
+    private Long sharePostId;
+    private String zipCode;
+    private String sharePostContent;
+    private List<ShareLetterPostResponse> letters;
+
+    @Builder
+    public SharePostDetailResponse(Long id, String zipCode, String sharePostContent, List<ShareLetterPostResponse> letters) {
+        this.sharePostId = id;
+        this.zipCode = zipCode;
+        this.sharePostContent = sharePostContent;
+        this.letters = letters != null ? letters : new ArrayList<>();
+    }
+
+}

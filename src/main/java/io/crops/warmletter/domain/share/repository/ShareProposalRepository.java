2014b55e8a46a7e42e155bf93df497b0220f5e6b
HeoJeongHyeon <jh7e0n20@gmail.com>
2025-02-22 02:30:52 +0900

    feat ShareProposalRepository 구현

--- src/main/java/io/crops/warmletter/domain/share/repository/ShareProposalRepository.java
+++ src/main/java/io/crops/warmletter/domain/share/repository/ShareProposalRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShareProposalRepository extends JpaRepository<ShareProposal,Long >, ShareProposalRepositoryCustom {
+public interface ShareProposalRepository extends JpaRepository<ShareProposal,Long >{
 
 }
--- src/main/java/io/crops/warmletter/domain/share/repository/ShareProposalRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package io.crops.warmletter.domain.share.repository;
-
-import io.crops.warmletter.domain.share.dto.response.ShareProposalResponse;
-
-public interface ShareProposalRepositoryCustom {
-    ShareProposalResponse findShareProposalWithZipCode(Long shareProposalId);
-}

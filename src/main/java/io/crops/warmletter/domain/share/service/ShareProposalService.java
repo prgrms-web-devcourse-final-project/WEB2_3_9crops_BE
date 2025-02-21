b9421a5f8d3e636c06bdeadb18777d4e10eb6b2e
HeoJeongHyeon <jh7e0n20@gmail.com>
2025-02-21 12:02:52 +0900

    refactor ShareProposalService
    - null 체크 예외 처리 추가

--- src/main/java/io/crops/warmletter/domain/share/service/ShareProposalService.java
+++ src/main/java/io/crops/warmletter/domain/share/service/ShareProposalService.java
@@ -6,6 +6,8 @@ import io.crops.warmletter.domain.share.entity.ShareProposal;
 import io.crops.warmletter.domain.share.entity.ShareProposalLetter;
 import io.crops.warmletter.domain.share.repository.ShareProposalLetterRepository;
 import io.crops.warmletter.domain.share.repository.ShareProposalRepository;
+import io.crops.warmletter.global.error.common.ErrorCode;
+import io.crops.warmletter.global.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +25,12 @@ public class ShareProposalService {
     @Transactional
     public ShareProposalResponse requestShareProposal(ShareProposalRequest request) {
         // 1. ShareProposal 저장
+        if (request.getRequesterId() == null || request.getRecipientId() == null ||
+                request.getLetters() == null || request.getLetters().isEmpty()) {
+            throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE);
+        }
         ShareProposal shareProposal = shareProposalRepository.save(request.toEntity());
+
 
         // 2. ShareProposalLetter 생성 및 저장
         List<ShareProposalLetter> letters = request.getLetters().stream()
@@ -32,6 +39,11 @@ public class ShareProposalService {
         shareProposalLetterRepository.saveAll(letters);
 
         // 3. Response 조회 및 반환
-        return shareProposalRepository.findShareProposalWithZipCode(shareProposal.getId());
+        ShareProposalResponse response = shareProposalRepository.findShareProposalWithZipCode(shareProposal.getId());
+        if (response == null) {
+            throw new BusinessException(ErrorCode.SHARE_POST_NOT_FOUND);
+        }
+        return response;
+
     }
 }

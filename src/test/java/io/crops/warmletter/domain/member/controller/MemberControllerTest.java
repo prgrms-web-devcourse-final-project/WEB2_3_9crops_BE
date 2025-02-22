4ebccb5fe9e2e7eb511c233a2063a292ec202136
leeys9423 <leeys9423@gmail.com>
2025-02-23 00:05:54 +0900

    test: 마이페이지 조회 테스트 추가 (WR9-52)

--- src/test/java/io/crops/warmletter/domain/member/controller/MemberControllerTest.java
+++ src/test/java/io/crops/warmletter/domain/member/controller/MemberControllerTest.java
@@ -1,6 +1,8 @@
 package io.crops.warmletter.domain.member.controller;
 
+import io.crops.warmletter.domain.member.dto.response.MeResponse;
 import io.crops.warmletter.domain.member.dto.response.ZipCodeResponse;
+import io.crops.warmletter.domain.member.enums.SocialProvider;
 import io.crops.warmletter.domain.member.exception.DuplicateZipCodeException;
 import io.crops.warmletter.domain.member.exception.MemberNotFoundException;
 import io.crops.warmletter.domain.member.service.MemberService;
@@ -20,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -84,5 +87,41 @@ class MemberControllerTest {
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$.code").value("MEM-002"))
                 .andExpect(jsonPath("$.message").value("우편번호가 이미 존재합니다."));
+    }
+
+    @DisplayName("마이페이지 조회 API 호출 성공")
+    @Test
+    void getMe_Success() throws Exception {
+        //given
+        String zipCode = "1A2AC";
+        float temperature = 36.5f;
+        SocialProvider social = SocialProvider.GOOGLE;
+        String email = "test@test.com";
+        MeResponse response = new MeResponse(zipCode, temperature, social, email);
+
+        when(memberService.getMe()).thenReturn(response);
+
+        //when & then
+        mockMvc.perform(get("/api/members/me"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.zipCode").value(zipCode))
+                .andExpect(jsonPath("$.data.temperature").value(temperature))
+                .andExpect(jsonPath("$.data.social").value(SocialProvider.GOOGLE.name()))
+                .andExpect(jsonPath("$.data.email").value(email))
+                .andExpect(jsonPath("$.message").value("마이페이지 조회 완료"));
+    }
+
+    @DisplayName("마이페이지 조회 API 호출 실패 - 존재하지 않은 사용자")
+    @Test
+    void getMe_Fail_MemberNotFound() throws Exception {
+        //given
+        when(memberService.getMe())
+                .thenThrow(new MemberNotFoundException());
+
+        //when & then
+        mockMvc.perform(get("/api/members/me"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code").value("MEM-001"))
+                .andExpect(jsonPath("$.message").value("유저를 찾을 수 없습니다."));
     }
 }

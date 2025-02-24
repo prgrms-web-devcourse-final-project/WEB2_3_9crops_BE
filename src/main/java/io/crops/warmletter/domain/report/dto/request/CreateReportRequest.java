284069ef7122792cb4dcb186396f88bea8692d40
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2025-02-24 16:23:54 +0900

    docs: ReportType 주석 가독성 개선 (WR9-50)

--- src/main/java/io/crops/warmletter/domain/report/dto/request/CreateReportRequest.java
+++ src/main/java/io/crops/warmletter/domain/report/dto/request/CreateReportRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreateReportRequest {
 
-    private ReportType reportType; // 신고 유형 (LETTER, SHARE_BOARD, EVENT_COMMENT)
+    private ReportType reportType; // 신고 유형 (LETTER, SHARE_POST, EVENT_COMMENT)
     private ReasonType reasonType;
     private String reason;
     private Long letterId;

8e5a589fa9e48f7bdcd82bff1c65abe44f06c611
HeoJeongHyeon <jh7e0n20@gmail.com>
2025-02-24 10:43:12 +0900

    feat 좋아요 요청 DTO
    - 필수값만

---
+++ src/main/java/io/crops/warmletter/domain/share/dto/request/SharePostLikeRequest.java
@@ -0,0 +1,17 @@
+package io.crops.warmletter.domain.share.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SharePostLikeRequest {
+
+    private Long sharePostId;
+    private Long memberId;
+
+}

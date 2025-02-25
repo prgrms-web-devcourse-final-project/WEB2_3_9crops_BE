65d95a833d87c528b11706f4fe8f4fae2f2e3fa7
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2025-02-25 16:28:34 +0900

    feat: report 상세목록 조회

--- src/main/java/io/crops/warmletter/domain/report/repository/ReportRepository.java
+++ src/main/java/io/crops/warmletter/domain/report/repository/ReportRepository.java
@@ -3,7 +3,7 @@ package io.crops.warmletter.domain.report.repository;
 import io.crops.warmletter.domain.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReportRepository extends JpaRepository<Report, Long> {
+public interface ReportRepository extends JpaRepository<Report, Long>, ReportRepositoryCustom {
     boolean existsByLetterIdAndMemberId(Long letterId, Long MemberId);
     boolean existsBySharePostIdAndMemberId(Long shareId, Long MemberId);
     boolean existsByEventCommentIdAndMemberId(Long eventCommentId, Long MemberId);

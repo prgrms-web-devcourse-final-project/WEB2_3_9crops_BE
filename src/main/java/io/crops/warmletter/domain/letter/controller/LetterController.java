6debeb6c75710dd73c22998613e0841c70988684
leeys9423 <leeys9423@gmail.com>
2025-02-25 17:06:32 +0900

    refactor: 공통 URI RequestMapping으로 설정 (WR9-67)

--- src/main/java/io/crops/warmletter/domain/letter/controller/LetterController.java
+++ src/main/java/io/crops/warmletter/domain/letter/controller/LetterController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/letters")
+@RequestMapping("/api")
 public class LetterController {
 
     private final LetterService letterService;
@@ -24,7 +24,7 @@ public class LetterController {
      * 편지를 처음 쓰는지
      * 답장을 보내는지
      */
-    @PostMapping
+    @PostMapping("/letters")
     public ResponseEntity<BaseResponse<LetterResponse>> createLetter(@RequestBody @Valid CreateLetterRequest request) {
         LetterResponse letterResponse = letterService.createLetter(request);
         BaseResponse<LetterResponse> response = BaseResponse.of(letterResponse, "편지가 성공적으로 생성되었습니다.");
@@ -35,7 +35,7 @@ public class LetterController {
     /**
      * 지정된 letterId의 이전 편지를 조회합니다.
      */
-    @GetMapping("/v1/{letterId}/previous")
+    @GetMapping("/v1/letters/{letterId}/previous")
     public ResponseEntity<BaseResponse<List<LetterResponse>>> getPreviousLetters(@PathVariable Long letterId) {
         List<LetterResponse> previousLetters = letterService.getPreviousLetters(letterId);
         BaseResponse<List<LetterResponse>> response = BaseResponse.of(previousLetters, "이전 편지가 전송 완료.");
@@ -45,7 +45,7 @@ public class LetterController {
     /**
      * 지정된 letterId 삭제 (softDelete)
      */
-    @DeleteMapping("/{letterId}")
+    @DeleteMapping("/letters/{letterId}")
     public ResponseEntity<BaseResponse<Void>> deleteLetter(@PathVariable Long letterId) {
         letterService.deleteLetter(letterId);
         BaseResponse<Void> response = BaseResponse.of(null, "편지 삭제 완료");
@@ -56,10 +56,16 @@ public class LetterController {
     /**
      * 지정된 letterId로 편지 단건 조회
      */
-    @GetMapping("/{letterId}")
+    @GetMapping("/letters/{letterId}")
     public ResponseEntity<BaseResponse<LetterResponse>> getLetterById(@PathVariable Long letterId) {
         LetterResponse letterResponse = letterService.getLetterById(letterId);
         BaseResponse<LetterResponse> response = BaseResponse.of(letterResponse, "편지 조회 완료");
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/letters/{letterId}/evaluate")
+    public ResponseEntity<BaseResponse<Void>> evaluateLetter(@PathVariable Long letterId) {
+        letterService.evaluateLetter(letterId);
+        return ResponseEntity.ok(BaseResponse.of(null, "편지 평가 완료"));
     }
 }

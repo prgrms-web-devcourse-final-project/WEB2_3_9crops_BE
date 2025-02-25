72a917e522e4f32e7ccb62a2e4e7e605674a9172
HeoJeongHyeon <jh7e0n20@gmail.com>
2025-02-25 16:23:08 +0900

    refactor 쿼리 수정
    - 기존 서브쿼리를 통해 사용자의 데이터가 존재하지않으면 Null값으로 Response 객체에 매핑되는 과정에서 False로 자동 변환되게 해놨었음.
    - 팀원분의 의견으로 CASE를 활용하면 True/False와 같은 명확한 boolean값을 제공해주므로, 이왕이면 명확하게 해결해주는 것이 더 좋은 프로그래밍 방향이라고 생각해서 리펙토링하였음.

--- src/main/java/io/crops/warmletter/domain/share/repository/SharePostLikeRepository.java
+++ src/main/java/io/crops/warmletter/domain/share/repository/SharePostLikeRepository.java
@@ -15,8 +15,9 @@ public interface SharePostLikeRepository extends JpaRepository<SharePostLike,Lon
     Optional<SharePostLike> findBySharePostIdAndMemberId(Long sharePostId, Long memberId);
     @Query("SELECT new io.crops.warmletter.domain.share.dto.response.SharePostLikeResponse(" +
             "COUNT(CASE WHEN spl.isLiked = true THEN 1 ELSE NULL END), " +
-            "(SELECT s.isLiked FROM SharePostLike s " +
-            " WHERE s.sharePostId = :sharePostId AND s.memberId = :memberId)) " +
+            "CASE WHEN EXISTS (SELECT 1 FROM SharePostLike s " +
+            "      WHERE s.sharePostId = :sharePostId AND s.memberId = :memberId AND s.isLiked = true) " +
+            "     THEN true ELSE false END) " +
             "FROM SharePostLike spl " +
             "WHERE spl.sharePostId = :sharePostId")
     SharePostLikeResponse getLikeCountAndStatus(@Param("sharePostId") Long sharePostId,

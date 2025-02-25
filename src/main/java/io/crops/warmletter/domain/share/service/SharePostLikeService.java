98c89caef069bb2121664a2d146646a197290744
HeoJeongHyeon <jh7e0n20@gmail.com>
2025-02-25 15:17:15 +0900

    feat 좋아요 기능
    controller
    service
    Repository

--- src/main/java/io/crops/warmletter/domain/share/service/SharePostLikeService.java
+++ src/main/java/io/crops/warmletter/domain/share/service/SharePostLikeService.java
@@ -1,6 +1,9 @@
 package io.crops.warmletter.domain.share.service;
 
 import io.crops.warmletter.domain.share.cache.PostLikeRedisManager;
+import io.crops.warmletter.domain.share.dto.response.SharePostLikeResponse;
+import io.crops.warmletter.domain.share.exception.SharePostNotFoundException;
+import io.crops.warmletter.domain.share.repository.SharePostLikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,14 @@ public class SharePostLikeService {
 
     private final PostLikeRedisManager postLikeRedisManager;
 
+    private final SharePostLikeRepository sharePostLikeRepository;
     public void toggleLike(Long postId, Long memberId) {
         postLikeRedisManager.toggleLike(postId, memberId);
     }
+    public SharePostLikeResponse getLikeCountAndStatus(Long sharePostId, Long memberId) {
+        if (sharePostId == null)
+            throw new SharePostNotFoundException();
 
+        return sharePostLikeRepository.getLikeCountAndStatus(sharePostId, memberId);
+    }
 }

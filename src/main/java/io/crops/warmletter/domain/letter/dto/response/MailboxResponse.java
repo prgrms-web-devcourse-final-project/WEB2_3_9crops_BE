5ea922becf6adeb9d212e74525e6b7094defbfe2
고진영 <jim00011128@gmail.com>
2025-03-02 02:01:59 +0900

    feat: 내 편지함 목록 조회 서비스단 로직 변경, 레파지토리 쿼리 추가 (WR9-69)
    - findMailboxDetails 추가로 기존 n+1 문제 해결
    - dto 추가 요청사항으로 필드 추가

--- src/main/java/io/crops/warmletter/domain/letter/dto/response/MailboxResponse.java
+++ src/main/java/io/crops/warmletter/domain/letter/dto/response/MailboxResponse.java
@@ -1,6 +1,7 @@
 package io.crops.warmletter.domain.letter.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,12 +10,21 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MailboxResponse {
 
-    private final Long letterMatchingId;
+    private final Long letterMatchingId; //편지 매칭 id
 
-    private final String oppositeZipCode;
+    private final String oppositeZipCode; //상대방의 zipcode
 
-    private final boolean isActive;
+    private final boolean isActive; //편지 매칭에서 활성 여부 방이 차단됐는지 안됐는지 true(활성), false(비활성)
 
-    private final boolean isOppositeRead;
+    private final boolean isOppositeRead; //편지가 전부 다 읽어졌는지 아닌지 다 읽으면 true 안읽으면 false
 
+    private final Long letterCount; // 총편지 수
+
+    public MailboxResponse(Long letterMatchingId, String oppositeZipCode, boolean isActive, boolean isOppositeRead, Long letterCount) {
+        this.letterMatchingId = letterMatchingId;
+        this.oppositeZipCode = oppositeZipCode;
+        this.isActive = isActive;
+        this.isOppositeRead = isOppositeRead;
+        this.letterCount = letterCount;
+    }
 }
